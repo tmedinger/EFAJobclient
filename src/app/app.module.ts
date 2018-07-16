7aebe144eef622eb91b2c8c1a962e2f05a5d9d79
Jack Demaree <jackdemaree@Jacks-MacBook-Pro.local>
2018-07-16 13:41:15 -0400

    changed app-routing module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
