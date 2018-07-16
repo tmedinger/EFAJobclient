0108c117e8b241819935ba323b844f96d7509483
Evan Sondgerath <esondgerath@gmail.com>
2018-07-16 13:11:15 -0400

    Initial navbar footer component file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './ui/navbar/navbar.component';
+import { FooterComponent } from './ui/footer/footer.component';
+import { LayoutComponent } from './ui/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
