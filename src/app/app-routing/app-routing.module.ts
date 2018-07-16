1075ba688a71fdbafa1ad31e547ed6c3275e3094
Jack Demaree <jackdemaree@Jacks-MacBook-Pro.local>
2018-07-16 11:59:16 -0400

    create app-module

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+const routes = [];
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
