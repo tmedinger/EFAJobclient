0cf18868e6800b8fb99daff0c3818ba22b360dfe
Ryan Gabbert <rgabb08@gmail.com>
2018-07-16 11:55:11 -0400

    added auth and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
