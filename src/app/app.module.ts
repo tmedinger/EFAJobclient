1b39bd25ac3e2d0c6a419ce2f600b69eb4cf266f
Austin Rardin <ajeflaunch@gmail.com>
2018-07-16 11:57:10 -0600

    signup component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponentComponent
   ],
   imports: [
     BrowserModule
