ed0fbe0aff4f936324ac8f0324bf8b7a3ec10fd4
Eleven Fifty <elevenfifty@Elevens-MacBook-Air.local>
2018-07-17 17:00:18 -0400

    fixed a route error

--- src/app/employer/employer.module.ts
+++ src/app/employer/employer.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { EmployerDashboardComponent } from './employer-dashboard/employer-dashboard.component';
 import { NotificationComponent } from './employer-dashboard/notification/notification.component';
 import { EmployerRoutingModule } from './employer-routing/employer-routing.module';
+import { JobsEditComponent } from './employer-dashboard/jobs-edit/jobs-edit.component';
 
 @NgModule({
   imports: [
     CommonModule,
     EmployerRoutingModule
   ],
-  declarations: [EmployerDashboardComponent, NotificationComponent]
+  declarations: [EmployerDashboardComponent, NotificationComponent, JobsEditComponent]
 })
 export class EmployerModule { }
