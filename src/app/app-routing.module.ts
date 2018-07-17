1160f90997697de4433dc85dcffdef11c3122534
Ryan Gabbert <rgabb08@gmail.com>
2018-07-17 16:01:46 -0400

    updated auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentLoginComponent } from './auth/student-login/student-login.component';
+import { EmployerLoginComponent } from './auth/employer-login/employer-login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/student-login', pathMatch: 'full' },
+    { path: 'student-login', component: StudentLoginComponent },
+    { path: 'employer-login', component: EmployerLoginComponent },
+  ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
