ed82899378cd1cc92f53d2f2f612c8e08717e56e
masonseiler <masonseiler1@gmail.com>
2018-07-17 10:13:20 -0400

    created job.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FooterComponent } from './ui/footer/footer.component';
 import { LayoutComponent } from './ui/layout/layout.component';
 import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SignupComponentComponent } from './signup-component/signup-component.component';
 import { SharedModule } from './shared/shared.module';
 import { EmployerLoginComponent } from './auth/employer-login/employer-login.component';
 import { StudentLoginComponent } from './auth/student-login/student-login.component';
@@ -27,10 +26,8 @@ import { StudentLoginComponent } from './auth/student-login/student-login.compon
     BrowserModule,
     SharedModule,
     ReactiveFormsModule,
-    SignupComponentComponent
-  ],
-  imports: [
-    AppRoutingModule,
+    AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
