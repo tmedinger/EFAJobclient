3b75303a885d4156d9f786fa4c03f357713de1e5
masonseiler <masonseiler1@gmail.com>
2018-07-18 09:22:01 -0400

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployerSignupComponent } from './auth/employer-signup/employer-signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/employersignup', pathMatch: 'full' },
+    { path: 'employersignup', component: EmployerSignupComponent}
+      
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
