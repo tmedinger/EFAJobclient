c8ad891009c84fd73d865a2190dbca668d212726
Austin Rardin <ajeflaunch@gmail.com>
2018-07-18 16:50:29 -0600

    first merge of meeting

--- src/app/employer/employer-routing/employer-routing.module.ts
+++ src/app/employer/employer-routing/employer-routing.module.ts
@@ -1,13 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import {JobDetailsComponent} from '../../shared/job-details/job-details.component'
-import { RouterModule, Routes } from '@angular/router'
-
-const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
-
-=======
 import { RouterModule, Routes } from '@angular/router';
 
 import { EmployerLoginComponent } from '../../auth/employer-login/employer-login.component';
@@ -17,7 +9,6 @@ const routes: Routes = [
   { path: 'employerlogin', component: EmployerLoginComponent},
   { path: 'employersignup', component: EmployerSignupComponent}
 ];
->>>>>>> 62ae40cfdf753d3ef1630e5e7c0ec3007dcf3724
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
