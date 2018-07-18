044ece54b3537fca918ee38d714a93fd5af13b3a
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-07-18 13:13:31 -0400

    changes

--- src/app/employer/employer-routing/employer-routing.module.ts
+++ src/app/employer/employer-routing/employer-routing.module.ts
@@ -1,25 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import {JobDetailsComponent} from '../../shared/job-details/job-details.component'
-
-const route = [ /* ROUTES WILL GO HERE */ 
-{ path: 'job-details/:id', component: JobDetailsComponent}
-=======
 import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' }
-<<<<<<< HEAD
-
-<<<<<<< HEAD
->>>>>>> 28fda87f474aaccaa799698aea97eb97f756eb3e
-=======
->>>>>>> beb868bbfa0cd80c25da901c427153fa09444dab
->>>>>>> develop
-=======
->>>>>>> 6f400c30e977e9df2becf0f708c1b11df50c2445
-];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
