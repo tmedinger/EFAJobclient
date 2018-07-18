12865561d447828698a1ff34076e84f645ee7ddc
marreynolds <marreynolds13@gmail.com>
2018-07-18 14:24:50 -0400

    fixed typo

--- src/app/employer/employer-routing/employer-routing.module.ts
+++ src/app/employer/employer-routing/employer-routing.module.ts
@@ -3,11 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-<<<<<<< HEAD
-  
-=======
   { path: '', redirectTo: '/', pathMatch: 'full' }
->>>>>>> 96deee37eb63b50f4ad6a569b7495120363da8ee
 ];
 
 @NgModule({
