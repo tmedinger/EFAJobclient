58cbe947f653b61de100b70dfbd51b3d15dd81e0
masonseiler <masonseiler1@gmail.com>
2018-07-19 15:51:22 -0400

    updates

--- src/app/student/student-routing/student-routing.module.ts
+++ src/app/student/student-routing/student-routing.module.ts
@@ -5,13 +5,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { StudentLoginComponent } from '../../auth/student-login/student-login.component';
 import { StudentSignupComponent } from '../../auth//student-signup/student-signup.component';
 import { StudentDashboardComponent } from '../student-dashboard/student-dashboard/student-dashboard.component';
-import { JobDetailsComponent } from '../../shared/job-details/job-details.component';
 
 const routes: Routes = [
   { path: 'studentlogin', component: StudentLoginComponent},
   { path: 'studentsignup', component: StudentSignupComponent},
   { path: 'studentdashboard', component: StudentDashboardComponent},
-  { path: 'jobdetails', component: JobDetailsComponent}
 ];
 
 @NgModule({
