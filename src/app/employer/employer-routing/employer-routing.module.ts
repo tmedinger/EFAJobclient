29562a93c89eb5dba7342ed60d042b15fcf64aca
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-07-19 10:49:21 -0400

    commented out img card

--- src/app/employer/employer-routing/employer-routing.module.ts
+++ src/app/employer/employer-routing/employer-routing.module.ts
@@ -1,14 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import {JobDetailsComponent} from '../../shared/job-details/job-details.component'
 import { RouterModule, Routes } from '@angular/router'
-
-const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' }
-=======
-import { RouterModule, Routes } from '@angular/router';
-
 import { EmployerLoginComponent } from '../../auth/employer-login/employer-login.component';
 import { EmployerSignupComponent } from '../../auth//employer-signup/employer-signup.component';
 
@@ -16,7 +8,6 @@ const routes: Routes = [
   { path: 'employerlogin', component: EmployerLoginComponent},
   { path: 'employersignup', component: EmployerSignupComponent}
 ];
->>>>>>> adbc3a605ea812171dc8b4440e3a0263f8ec0767
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
