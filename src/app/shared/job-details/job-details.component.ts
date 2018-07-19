e60a03887f5da1436c6c1d03e5d65fcd3df42432
Iesha Duff <ieshaduff@Ieshas-MacBook-Pro.local>
2018-07-19 10:24:35 -0400

    finished job details card

--- src/app/shared/job-details/job-details.component.ts
+++ src/app/shared/job-details/job-details.component.ts
@@ -20,8 +20,8 @@ export class JobDetailsComponent implements OnInit {
     public datePosted: string,
     public listOfApplicants: string[],
     public apply:boolean,
-    public create:string,
-    public updated: string,
+    public create:boolean,
+    public update: boolean,
     public dialog: MatDialog
   ) { }
 
