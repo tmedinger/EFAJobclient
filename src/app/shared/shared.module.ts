3676d557148cb15ac87a7a29bbc2c7266f452bd5
Tony <tmedinger@gmail.com>
2018-07-20 10:34:32 -0400

    minor changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -19,7 +19,6 @@ import {
         MatRadioModule,
         MatSidenavModule,
         MatSlideToggleModule,
-        MatSnackBar,
         MatTableModule,
         MatToolbarModule,
         MatTooltipModule, 
